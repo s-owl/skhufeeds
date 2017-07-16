4c482788e12a0e0e09e92c0b0afb30313ab92475
nr970810 <nr970810@gmail.com>
2017-07-16 16:22:53 +0900

    í¬ë¡¤ë¬ ëª¨ë¸ ì¶ê°

--- skhufeeds/crawlers/models.py
+++ skhufeeds/crawlers/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
+#크롤링 데이터 분류를 위한 모델
+class Sources(models.Model):
+    id = models.AutoField()
+    name = models.CharField(max_length=45)
+    url = models.UrlField()
+    desc = models.TextField()
+
+#피드 데이터 모델
+class NewsFeeds(models.Model):
+    id = models.AutoField()
+    time = models.TimeField
+    title = models.CharField(max_length = 45)
+    summary = models.CharField(max_length = 100)
+    url = models.UrlField
+    source_id = models.ForeignKey(
+        'Sources',
+        on_delete = models.PROTECT,
+    )
+
+#연락처 데이터 모델
+class Phone(models.Model)
+    id = models.AutoField()
+    name = models.CharField(max_length = 45)
+    desc = models.TextField()
+    phone = models.CharField(max_length = 45)
+    email = models.EmailField()
