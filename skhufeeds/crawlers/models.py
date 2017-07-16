b87e74372354023ce1915a6242a2c29e1aeff4cd
sukso96100 <sukso96100@gmail.com>
2017-07-16 16:42:02 +0900

    크롤러 모델 오류 수정

--- skhufeeds/crawlers/models.py
+++ skhufeeds/crawlers/models.py
@@ -3,26 +3,26 @@ from django.db import models
 #Create your models here.
 #크롤링 데이터 분류를 위한 모델
 class Sources(models.Model):
-    id = models.AutoField()
+    source_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
-    url = models.UrlField()
+    url = models.URLField()
     desc = models.TextField()
 
 #피드 데이터 모델
 class NewsFeeds(models.Model):
-    id = models.AutoField()
-    time = models.TimeField
+    news_id = models.AutoField(primary_key=True)
+    time = models.TimeField()
     title = models.CharField(max_length = 45)
     summary = models.CharField(max_length = 100)
-    url = models.UrlField
+    url = models.URLField()
     source_id = models.ForeignKey(
         'Sources',
-        on_delete = models.PROTECT,
+        on_delete = models.PROTECT
     )
 
 #연락처 데이터 모델
-class Phone(models.Model)
-    id = models.AutoField()
+class Phone(models.Model):
+    phone_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length = 45)
     desc = models.TextField()
     phone = models.CharField(max_length = 45)
