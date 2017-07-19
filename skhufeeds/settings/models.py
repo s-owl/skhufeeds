0803e3eb3a1d7e098cd5f1753fd1da68bf3238f7
sukso96100 <sukso96100@gmail.com>
2017-07-19 15:43:50 +0900

    updated kakao module url

--- skhufeeds/settings/models.py
+++ skhufeeds/settings/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-# 사용자 데이터 모델
-class Subscribers(models.Model):
-    user_id = models.CharField(max_length=45)
 
-# 사용자 구독정보 모델
-class SubscribeList(models.Model):
-    user_id = models.ForeignKey('Subscribers', on_delete = models.CASCADE)
-    source_id = models.ForeignKey('crawlers.Sources', on_delete = models.CASCADE)
+# class UserInfo(models.Model):
+#     last_pull = models.DateTimeField()
+#
+# # 사용자 구독정보 모델
+# class SubscribeList(models.Model):
+#     user_id = models.ForeignKey('Subscribers', on_delete = models.CASCADE)
+#     source_id = models.ForeignKey('crawlers.Sources', on_delete = models.CASCADE)
