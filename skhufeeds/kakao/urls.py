3e1a7d64117c72c4cd81b6eaf9940654e571d7b1
김희수 <hisu138@gmail.com>
2017-07-21 19:14:20 +0900

    update

--- skhufeeds/kakao/urls.py
+++ skhufeeds/kakao/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^keyboard/$', views.keyboard),
     url(r'^message$', views.answer),
-    url(r'^friend/$', views.add_friend),
+    url(r'^friend$', views.add_friend),
     url(r'^friend/(?P<user_key>\w+)$', views.del_friend)
     # url(r'^answer/', views.answer)
     #위의 urls.py와는 달리 include가 없습니다.
