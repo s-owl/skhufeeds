03792cd32604b4513c7d2ce573433ff0c6376896
nr970810 <nr970810@gmail.com>
2017-07-27 16:45:16 +0900

    add Info files

---
+++ skhufeeds/crawlers/crawlers/info/welfare_student.py
@@ -0,0 +1,21 @@
+#성공회대학교 대학기구_학생복지처 페이지 긁어오기
+from urllib.request import urlopen
+from bs4 import BeautifulSoup, Tag
+html = urlopen("http://www.skhu.ac.kr/uni_int/uni_int_5_4.aspx")
+bs0bj = BeautifulSoup(html.read(),"html.parser")
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.skhu.ac.kr/uni_int/uni_int_5_4.aspx")
+bj0bj = BeautifulSoup(html, "html.parser")
+
+main = "대표전화 02)2610-4114 / 팩스 02)2683-8858/ 야간전화 02)2610-4119/ 직통전화 02)2610 + 교내번호"
+print(main)
+
+
+for child in bs0bj.find("table",{"class":"cont_a mt20 ml20 w690"}).tbody.children:
+    if isinstance(child, Tag):
+        item = child.findAll("td")
+        print(item[0].get_text() ,item[1].get_text(),item[2].get_text(),item[3].get_text(),item[4].get_text(),item[5].get_text())
