63c4a79aaaf22088a380d70163495ed4393cb589
sukso96100 <sukso96100@gmail.com>
2017-07-31 14:05:35 +0900

    last_command field is not nullable

--- skhufeeds/settings/models.py
+++ skhufeeds/settings/models.py
@@ -6,7 +6,7 @@ class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     last_pull = models.DateTimeField(auto_now_add=False, auto_now=False)
     token = models.TextField()
-    last_command = models.CharField(max_length=10)
+    last_command = models.CharField(max_length=10, null=True)
 #
 # 사용자 구독정보 모델
 class SubscribeList(models.Model):
