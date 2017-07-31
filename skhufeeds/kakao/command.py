f15fb71a1c9bba40650afacec4222972b2248335
김희수 <hisu138@gmail.com>
2017-07-31 15:36:09 +0900

    Phone filter

--- skhufeeds/kakao/command.py
+++ skhufeeds/kakao/command.py
@@ -5,7 +5,8 @@ import json, datetime
 from skhufeeds import account
 from . import command
 from django.contrib.auth.models import User
-
+from settings.models import UserInfo
+from crawlers.models import Phone
 
 default = ['학교소식','연락처','날씨','학식','설정']
 
@@ -13,12 +14,13 @@ default = ['학교소식','연락처','날씨','학식','설정']
 def answer(request):
     json_str = ((request.body).decode('utf-8'))
     received_json_data = json.loads(json_str)
-    today_date = datetime.date.today().strftime("%m월 %m일")
+    today_date = datetime.date.today().strftime("%m월 %d일")
     command = received_json_data['content']
     user_key = received_json_data['user_key']
 
     try:
         user = User.objects.get(username = user_key)
+        userinfo = UserInfo.objects.get(user = user)
     except User.DoesNotExist:
         return JsonResponse({
             'message' : {
@@ -33,7 +35,7 @@ def answer(request):
     if(command == '학식'):
         return JsonResponse({
             'message' : {
-                'text': today_date + ' 중식 메뉴: 없음'
+                'text': today_date + '중식: 없음\n석식: 없음'
             },
             'keyboard': {
                 'type' : 'buttons',
@@ -66,7 +68,7 @@ def answer(request):
         return JsonResponse({
 
             'message' : {
-                'text': '무엇으로 검색하시겠습니까?'
+                'text': '무엇으로 검색하시겠습니까?\n연락처는 내선번호와 이메일이 제공됩니다.'
             },
             'keyboard': {
                 'type' : 'buttons',
@@ -89,25 +91,74 @@ def answer(request):
                 'buttons' : default
             }
         })
-    elif(command == '성명'):
+    elif(command == '성명''):
         return JsonResponse({
-            'message' : {
-                'text': '아직 구현되지 않았습니다.'
-            },
-            'keyboard': {
-                'type' : 'buttons',
-                'buttons' : default
+            'message':{
+                'text': '교수명을 입력하세요.'
+            }
+            'keyboard' :{
+                'type' : 'text'
             }
         })
     elif(command == '소속'):
         return JsonResponse({
-            'message' : {
-                'text': '아직 구현되지 않았습니다.'
-            },
-            'keyboard': {
-                'type' : 'buttons',
-                'buttons' : default
+            'message':{
+                'text': '학과명 또는 부서명을 입력하세요.'
+            }
+            'keyboard' :{
+                'type' : 'text'
             }
         })
+    elif(userinfo.last_command == '성명'):
+        result = Phone.objects.filter(name = name)
+        msg = ""
+        for item in result:
+            msg += '{}:\n   내선번호: {}\n  e-mail: {}\n'.format(Phone.name,Phone.phone,Phone.email)
+        if(len(result)==0):
+            return JsonResponse({
+                'message' : {
+                    'text': '해당 교수명이 존재하지 않습니다.'
+                },
+                'keyboard': {
+                    'type' : 'buttons',
+                    'buttons' : ['성명','소속']
+                }
+            })
+        else:
+            return JsonResponse({
+                'message':{
+                    'text': msg
+                }
+                'keyboard':{
+                    'type' : 'buttons',
+                    'buttons' : default
+                }
+            })
+
+    elif(userinfo.last_command == '소속'):
+        result2 = Phone.objects.filter(desc__contains = desc)
+        msg2 = ""
+        for item2 in result:
+            msg2 += '{}:\n   내선번호: {}\n  e-mail: {}\n'.format(Phone.name,Phone.phone,Phone.email)
+        if(len(result2)==0):
+            return JsonResponse({
+                'message' : {
+                    'text': '해당 학과명 또는 부서명이 존재하지 않습니다.'
+                },
+                'keyboard': {
+                    'type' : 'buttons',
+                    'buttons' : ['성명','소속']
+                }
+            })
+        else:
+            return JsonResponse({
+                'message':{
+                    'text': msg2
+                }
+                'keyboard':{
+                    'type' : 'buttons',
+                    'buttons' : default
+                }
+            })
     else:
         return HttpResponseNotFound
