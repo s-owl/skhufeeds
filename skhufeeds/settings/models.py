cd37f9f13b5509f7084e3a85e7e47db49f34fa80
sukso96100 <sukso96100@gmail.com>
2017-07-31 22:52:19 +0900

    revert t Please enter the commit message for your changes. Lines starting

--- skhufeeds/settings/models.py
+++ skhufeeds/settings/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserInfo(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     last_pull = models.DateTimeField(auto_now_add=False, auto_now=False)
     token = models.TextField(default="")
     secret = models.TextField(default="")
