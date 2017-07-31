1abfb4d83baab4ae727a4fc162f94199c4e25bab
sukso96100 <sukso96100@gmail.com>
2017-07-31 14:02:26 +0900

    fixed typo

--- skhufeeds/skhufeeds/authbackend.py
+++ skhufeeds/skhufeeds/authbackend.py
@@ -6,7 +6,7 @@ import jwt
 class UrlTokenBackend(ModelBackend):
     def authenticate(self, useruid, token):
         user = User.objects.get(username = useruid)
-        userInfo = UserInfo.get(user = user)
+        userInfo = UserInfo.objects.get(user = user)
         if(userInfo.token == token):
             jwt.decode(token, user.password, audience=useruid)
             return user
