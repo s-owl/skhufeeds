f9a80b68aa05ce00de55f505287aab6dbee9d621
sukso96100 <sukso96100@gmail.com>
2017-08-15 00:57:54 -0400

    added user settings index page skeleton

--- skhufeeds/settings/views.py
+++ skhufeeds/settings/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpResponseNotFound, HttpResponseForbidden
-import jwt
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 ## authenticate user with useruid and jwt token
@@ -17,4 +16,9 @@ def authUser(request, useruid, token):
         return HttpResponseForbidden("만료된 URL 입니다.")
     else:
         login(request, user)
-        return HttpResponse("로그인 성공")
+        return HttpResponseRedirect("/") # Redirect user to index page of settings app
+
+@login_required
+def index(request):
+    user = request.user
+    return HttpResponse("Hello, {}".format(user.username))
