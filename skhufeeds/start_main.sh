2ba843929ae1a88c35b3c85b8b4d66919f00059d
sukso96100 <sukso96100@gmail.com>
2017-09-07 22:15:04 +0900

    gunicorn

--- skhufeeds/start_main.sh
+++ skhufeeds/start_main.sh
@@ -4,4 +4,4 @@ python3 manage.py makemigrations skhufeeds
 python3 manage.py makemigrations settings
 python3 manage.py makemigrations crawlers
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:3000
+gunicorn -b 0.0.0.0:3000 skhufeeds.wsgi
