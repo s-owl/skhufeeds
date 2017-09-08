0a3d4b4434c070f1392c47fea01c9f32f002a7aa
sukso96100 <sukso96100@gmail.com>
2017-09-08 14:04:44 +0900

    static file serving

--- skhufeeds/start_main.sh
+++ skhufeeds/start_main.sh
@@ -4,4 +4,5 @@ python3 manage.py makemigrations skhufeeds
 python3 manage.py makemigrations settings
 python3 manage.py makemigrations crawlers
 python3 manage.py migrate
+python3 manage.py collectstatic --no-input
 gunicorn -b 0.0.0.0:3000 skhufeeds.wsgi
