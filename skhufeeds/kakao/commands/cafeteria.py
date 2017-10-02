1ce24fb2f880052c09e709590dec79f1ce326cbc
h2su <hisu138@gmail.com>
2017-10-02 20:32:07 +0900

    cafeteria cmd add

--- skhufeeds/kakao/commands/cafeteria.py
+++ skhufeeds/kakao/commands/cafeteria.py
@@ -1,11 +1,22 @@
 from . import util
 from django.http import JsonResponse
-from crawlers.crawlers import menu
+from django.utils import timezone
+from crawlers.models import Diet
+
 def run(user,command,user_key):
     util.updateLastCommand(command,user.profile)
+    date = timezone.now()
+
+    result = Diet.objects.get(date__year=date.year, date__month=date.month, date__day=date.day)
+
+    if(result == None):
+        menu = "해당 요일에 학식이 없습니다."
+    else:
+        menu = "{} 메뉴\n중식A: {}\n중식B: {}\n석식: {}".format(result.date,result.lunchA,result.lunchB,result.dinner)
+
     return JsonResponse({
         'message' : {
-            'text': today_date + '\nmenu.run()'
+            'text': menu
         },
         'keyboard': {
             'type' : 'buttons',
