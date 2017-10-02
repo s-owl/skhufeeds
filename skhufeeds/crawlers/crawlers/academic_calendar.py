6766af9148dcc0b73c8524ef188434260a7afa15
nr970810 <nr970810@gmail.com>
2017-10-02 15:42:44 +0900

    add remark

--- skhufeeds/crawlers/crawlers/academic_calendar.py
+++ skhufeeds/crawlers/crawlers/academic_calendar.py
@@ -3,16 +3,16 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup, Tag
 
 def run():
-    html = urlopen("http://www.skhu.ac.kr/calendar/calendar_list_1.aspx")
+    html = urlopen("http://www.skhu.ac.kr/calendar/calendar_list_1.aspx") #성공회대학교 학사일정 url
     bs0bj = BeautifulSoup(html.read(),"html.parser")
 
-    data = ""
+    data = "" #변수 선언 및 초기화
 
+    # for문을 사용하여 원하는 부분을 추출 및 출력
     for child in bs0bj.find("div",{"class","info"}).table.tbody.children:
-        if isinstance(child, Tag):
-            date = child.find("td",{"class","day"})
-            contents = child.find("td",{"class","txt"})
-            if isinstance(date, Tag):
-                # print(date.get_text(), contents.get_text())
-                data+="[{}] {}\n".format(date.get_text(), contents.get_text())
-    return data
+        if isinstance(child, Tag):#child의 타입이 Tag인지 확인
+            date = child.find("td",{"class","day"}) #날짜
+            contents = child.find("td",{"class","txt"}) #내용
+            if isinstance(date, Tag):#child의 타입이 Tag인지 확인
+                data+="[{}] {}\n".format(date.get_text(), contents.get_text()) #날짜와 행사내용
+    return data 
--- skhufeeds/crawlers/crawlers/menu.py
+++ skhufeeds/crawlers/crawlers/menu.py
@@ -1,20 +1,19 @@
-#성공회대학교 학생식당페이지 긁어오기
+#성공회대학교 학생식당_식단안내 페이지 긁어오기
 from urllib.request import urlopen
 from bs4 import BeautifulSoup, Tag
 
-
 def run():
-#     html = urlopen("http://www.skhu.ac.kr/uni_int/uni_int_5_4.aspx")
-#     bs0bj = BeautifulSoup(html.read(),"html.parser")
-#
-#     for child in bs0bj.find("table",{"class":"cont_c"}).tbody.children:
-#         if isinstance(child, Tag):
-#             item = child.findAll("td")
-#             print(item[0].get_text(),
-#             item[1].get_text(),
-#             item[2].get_text(),
-#             item[3].get_text(),
-#             item[4].get_text(),
-#             item[5].get_text())
-#
-    return None
+    html = urlopen("http://www.skhu.ac.kr/uni_zelkova/uni_zelkova_4_3_view.aspx?idx=315")#성공회대학교 학생식당 식단안내 url
+    bs0bj = BeautifulSoup(html.read(),"html.parser")
+    data = list()
+
+    # for문을 사용하여 원하는 부분을 추출 및 출력
+    for child in bs0bj.find("div",{"class":"box_menu_in"}).table.tbody.children:
+        if isinstance(child, Tag): #child의 타입이 Tag인지 확인
+            item = child.findAll("td")
+            print(item[0].get_text("\n"), #학생식단 출력
+            item[1].get_text("\n"),
+            item[2].get_text("\n"),
+            item[3].get_text("\n"),
+            item[4].get_text("\n"),
+            item[5].get_text("\n"))
