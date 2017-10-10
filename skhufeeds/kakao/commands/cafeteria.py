696c5118034e444c96cee1102150b2b272ca5677
sukso96100 <sukso96100@gmail.com>
2017-10-10 15:55:40 +0900

    fixed diet query and for loop index range

--- skhufeeds/kakao/commands/cafeteria.py
+++ skhufeeds/kakao/commands/cafeteria.py
@@ -2,17 +2,30 @@ from . import util
 from django.http import JsonResponse
 from django.utils import timezone
 from crawlers.models import Diet
+from datetime import datetime
 
 def run(user,command,user_key):
     util.updateLastCommand(command,user.profile)
     date = timezone.now()
 
     try:
-        result = Diet.objects.get(date__year=date.year, date__month=date.month, date__day=date.day)
-        menu = "{} 메뉴\n중식A: {}\n중식B: {}\n석식: {}".format(result.date,result.lunchA,result.lunchB,result.dinner)
+        # 오늘 날자에 해당하는 식단을 DB 에서 조회
+        result = Diet.objects.get(date__year=date.year,
+                                  date__month=date.month,
+                                  date__day=date.day)
+        # 문자열 조립
+        menu = "{}년 {}월 {}일 메뉴\n[중식A]\n{}\n\n[중식B]\n{}\n\n[석식]\n{}".format(
+            result.date.year,
+            result.date.month,
+            result.date.day+1,
+            result.lunchA,
+            result.lunchB,
+            result.dinner)
     except Diet.DoesNotExist:
+        # 없는경우, 안내 표시
         menu = "해당 요일에 학식이 없습니다."
 
+    # 클라이언트에 응답
     return JsonResponse({
         'message' : {
             'text': menu
